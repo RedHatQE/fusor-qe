06fc6465bfac99f7659a5dda13370f6a024e3e40
James Olin Oden <joden@redhat.com>
2016-06-20 13:48:53 -0400

    Add cancel modal buttons
    In insights, a modal frame will come up if you select cancel with
    the options:
    
        - cancel and delete
        - cancel and save
        - continue working

--- pages/wizard/satellite/insights.py
+++ pages/wizard/satellite/insights.py
@@ -19,6 +19,22 @@ class Insights(Base):
         '//button[contains(@class,"btn") and contains(., "Next")]'
     )
 
+    # These locators for the cancel model frame.   Note since the code
+    # is always there we don't need to select the dialogue as its just
+    # a frame in the existing page that sometimes is visible.
+    _exit_and_delete_loc = (
+        By.XPATH,
+        '//button[contains(.,"Exit and Delete")]'
+    )
+    _exit_and_save_loc = (
+        By.XPATH,
+        '//button[contains(.,"Exit and Save")]'
+    )
+    _continue_working_loc = (
+        By.XPATH,
+        '//button[contains(.,"Continue Working")]'
+    )
+
     # properties
     @property
     def enable(self):
@@ -36,15 +52,37 @@ class Insights(Base):
     def nextBtn(self):
         return self.selenium.find_element(*self._next_loc)
 
+    @property
+    def exit_and_delete_btn(self):
+        return self.selenium.find_element(*self._exit_and_delete_loc)
+
+    @property
+    def exit_and_save_btn(self):
+        return self.selenium.find_element(*self._exit_and_save_loc)
+
+    @property
+    def continue_working_btn(self):
+        return self.selenium.find_element(*self._continue_working_loc)
+
     # actions
     def click_enable(self):
         self.enable.click()
 
 
+    # Note a modal frame will open when this is clicked, with the
+    # buttons:
+    #
+    #   - Exit and Delete
+    #   - Exit and Save
+    #   - Continue Working
+    #
+    # These options may be selected by the corresponding calls:
+    #
+    #   - click_exit_and_delete()
+    #   - click_exit_and_save()
+    #   - click_continue_working()
     def click_cancel(self):
         self.cancelBtn.click()
-        # XXX: This needs some work as a second modal opens when you
-        #      click this.
 
 
     def click_back(self):
@@ -57,8 +95,20 @@ class Insights(Base):
         #      This is not trivial though and will be handled
         #      by another library.
 
+    def click_exit_and_delete(self):
+        self.exit_and_delete_btn.click()
+        return DashboardPage(self.base_url, self.selenium)
+
+    def click_exit_and_save(self):
+        self.exit_and_save_btn.click()
+        return DashboardPage(self.base_url, self.selenium)
+
+    def click_continue_working(self):
+        self.continue_working_btn.click()
+
 # These libraries are loaded so we can instantiate their objects after
 # the navigational buttons are clicked.
 # Also these libraries have to be loaded after our class is defined, because
 # we have circular dependencies on one another.
 from pages.wizard.satellite.update_availability import UpdateAvailability
+from pages.dashboard import DashboardPage
